e5f2433d28c820a4415bafad9ef224c74487e2f7
王 星然 <xingran.wang@onewonder.co.jp>
2024-12-15 21:39:25 +0900

    TEST

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
 resource "aws_iam_user" "db_users" {
-  # 使用 user 作为唯一键
-  for_each = {
-    for user_data in local.users : user_data.user => {
-      user   = user_data.user
-    }
-  }
+  for_each = { for user in local.users_data : user["IAMユーザー"] => user }
 
-  # IAM 用户名称
   name = each.key
 
-  # IAM 用户标签
   tags = {
-    Role = each.value.user
+    Policy = each.value["ポリシー"]  # 通过 "ポリシー" 获取策略名称
   }
 }
